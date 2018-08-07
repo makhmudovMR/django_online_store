13c31e635997ef313d50e760132c9ae6b8e293bf
Magomed Makhmudov <mmriadovich@gmail.com>
2018-08-07 23:03:29 +0300

    change views.py file

--- onlinestore/landing/views.py
+++ onlinestore/landing/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .forms import SubscribersForm
 
 def index(request):
-    return render(request, "landing/landing.html")
+    form = SubscribersForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        # print(request.POST)
+        # print(form.cleaned_data)
+        new_form = form.save()
+    return render(request, "landing/landing.html", {"form" : form})
