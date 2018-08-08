75747e616dfbfa9ca2b032e4d817d5952f7f1e9c
Magomed Makhmudov <mmriadovich@gmail.com>
2018-08-08 18:52:28 +0300

    change models.py and admin.py

--- onlinestore/landing/models.py
+++ onlinestore/landing/models.py
@@ -5,3 +5,6 @@ class Subscriber(models.Model):
 
     email = models.EmailField()
     name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return "User: %s %s" % (self.name, self.email)
