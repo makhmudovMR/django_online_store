a8a5f49af405a09057c0039894cc81351eac3de0
Magomed Makhmudov <mmriadovich@gmail.com>
2018-08-11 00:46:10 +0300

    admin and models

--- onlinestore/products/admin.py
+++ onlinestore/products/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import *
 
+
+class ProductImgInline(admin.TabularInline):
+    model = ProductImg
+    extra = 0
+
+
 class ProductAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'description']
+    inlines = [ProductImgInline]
 
     class Meta:
         model = Product
--- onlinestore/products/models.py
+++ onlinestore/products/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 
+
 class Product(models.Model):
 
     name = models.CharField(max_length=200, blank=True, null=True, default=None)
     description = models.TextField(max_length=1000, blank=True, null=True, default=None)
-
+    is_active = models.BooleanField(default=True)
 
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     class Meta:
 
@@ -18,17 +19,20 @@ class Product(models.Model):
         verbose_name_plural = 'Товары'
 
 
+'''--------------------------------------------------sep--------------------------------------------------'''
+
+
 class ProductImg(models.Model):
 
-    product = models.ForeignKey(Product) # link on product object or link on cell of table in database
-    img = models.ImageField(upload_to='/')
+    product = models.ForeignKey(Product, blank=True, null=True, default=None) # link on product object or link on cell of table in database
+    img = models.ImageField(upload_to='static/media/project_img/')
     is_active = models.BooleanField(default=True)
 
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
     class Meta:
 
